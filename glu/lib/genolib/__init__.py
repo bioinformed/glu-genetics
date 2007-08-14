f16074864845a219f500a10ca4ce35b5cc1685ba
jacobske <jacobske@localhost>
2007-08-14 21:05:38 +0000

    Analysis Ticket #221: Recovered from the !@#%!-failure that is SVN. glu.lib.genolib.binary lost its history, but otherwise I think I've recovered everything else.  Thus begins the end of our use of SVN for GLU. Mercurial, Darcs, or GIT are looking much better alternatives right now.

---
+++ glu/lib/genolib/__init__.py
@@ -0,0 +1,5 @@
+from streams import GenomatrixStream, GenotripleStream
+from io      import load_genostream, load_genomatrixstream, load_genotriplestream, \
+                    save_genostream, load_genomatrixstream, save_genotriples
+from reprs   import get_genorepr, snp
+from merge   import get_genomerger
