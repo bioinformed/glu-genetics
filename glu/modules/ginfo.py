cea21cb35939aeef7f2e2560534d0a4d38a41c0f
jacobske <jacobske@localhost>
2007-10-05 18:31:59 +0000

    Make ginfo less lazy and actually return what you request, even if it involves materialization.

--- glu/modules/ginfo.py
+++ glu/modules/ginfo.py
@@ -45,6 +45,8 @@ def option_parser():
                     help='Input genotype representation.  Values=snp (default), hapmap, marker')
   parser.add_option('-o', '--output', dest='output', metavar='FILE', default='-',
                     help='Output results (default is "-" for standard out)')
+  parser.add_option('-l', '--lazy', dest='lazy', action='store_true', default=False,
+                    help='Be lazy and never materialize the genotypes.  Some results may come back unknown')
   parser.add_option('--loci',   dest='loci',    metavar='FILE',
                      help='Output the list of loci to FILE')
   parser.add_option('--samples',dest='samples', metavar='FILE',
@@ -68,6 +70,10 @@ def main():
 
   out = autofile(hyphen(options.output,sys.stdout),'w')
 
+  if None in (genos.samples,genos.loci) and not options.lazy:
+    out.write('Materializing genotypes.\n')
+    genos = genos.materialize()
+
   out.write('Filename    : %s\n' % namefile(infile))
   out.write('Format      : %s\n' % genos.format)
 
@@ -85,8 +91,9 @@ def main():
     out.write('model  count: %d\n' % len(set(genos.models)))
     if options.models:
       def _models():
-        for locus,model in izip(genos.loci,genos.models):
+        for locus,model in genos.model_pairs:
           yield [locus, ','.join(sorted(model.alleles[1:]))]
+
       emit(options.models,_models())
 
 
