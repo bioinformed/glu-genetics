a78bc8ebe2867d86faaed6419303d36f70fe04ba
jacobske <jacobske@localhost>
2007-12-26 13:40:51 +0000

    Clean up of the union find algorithm with some new helper methods and improved documentation.  Suggestions based on feedback from D. Eppstein.

--- glu/lib/union_find.py
+++ glu/lib/union_find.py
@@ -19,23 +19,55 @@ __license__   = 'See GLU license for terms by running: glu license'
 
 class union_find(object):
   '''
-  A Class to implement union-find algorithm that performs two useful operations on a disjoint-set data structure:
+  A union_find instance X maintains a family of disjoint sets of
+  hashable objects, supporting the following two primary methods:
 
-    Find  : Determine which set a particular element is in and also if two elements are in the same set.
-    Union : Combine or merge sets into a single set.
+  - X[item] returns a name for the set containing the given item.
+    Each set is named by an arbitrarily-chosen one of its members; as
+    long as the set remains unchanged it will keep the same name. If
+    the item is not yet part of a set in X, a new singleton set is
+    created for it.
+
+  - X.union(item1, item2, ...) merges the sets containing each item
+    into a single larger set.  If any item is not yet part of a set
+    in X, it is added to X as one of the members of the merged set.
+
+  Based on code from Josiah Carlson with additional suggestions from D.
+  Eppstein.  See:
+
+      http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/215912
+
+  >>> uf = union_find()
+  >>> uf.union(1,2,3)
+  >>> uf[1] is uf[2]
+  True
+  >>> uf[2] is uf[3]
+  True
+
+  >>> uf.union(4,5)
+  >>> (4,5) in uf
+  True
+  >>> (1,4) in uf
+  False
+
+  >>> uf.union(3,5)
+  >>> (1,4) in uf
+  True
+
+  >>> uf.union(10,11)
+  >>> sorted( (ex,sorted(s)) for ex,s in uf.setmap().iteritems())
+  [(1, [1, 2, 3, 4, 5]), (10, [10, 11])]
   '''
   def __init__(self, labels=None):
     '''
     Construct a new Union_find object with given sets of elements if any.
     '''
-    if labels is not None:
-      self.labels = dict( (l,l) for l in labels )
-    else:
-      self.labels = {}
+    self.labels = dict( (l,l) for l in labels or [])
 
   def find(self, i, create=True):
     '''
-    Return the label for the specified element. If not existed, create a new label
+    Return the label for the specified element.  If create==False then the
+    item is not tracked as part of the universe of possible objects.
     '''
     labels = self.labels
 
@@ -56,17 +88,29 @@ class union_find(object):
 
     return j
 
+  def __getitem__(self, i):
+    '''
+    Return the label for the specified element.
+    '''
+    return self.find(i)
+
+  def union(self, *items):
+    '''
+    Union a sequence of items (len>1)
+    '''
+    if len(items) < 2:
+      raise ValueError('Union requires at least two items')
+
+    exemplars = (self.find(i) for i in items)
+    exemplar  = exemplars.next()
+    for i in exemplars:
+      self.labels[i] = exemplar
+
   def has_key(self, i):
     '''
     Return whether or not the element that was passed in exists in the union_find object
     '''
     return i in self.labels
-
-  def union(self, i1, i2):
-    '''
-    Merge sets into one set
-    '''
-    self.labels[self.find(i1)] = self.find(i2)
 
   def sets(self):
     '''
@@ -76,10 +120,10 @@ class union_find(object):
 
   def setmap(self):
     '''
-    Return a dictionary which contains a number of separate, nonoverlapping sets
+    Return a dictionary of exemplars to the contents of each disjoint set
     '''
     sets = {}
-    for i in self.labels:
+    for i in self:
       j = self.find(i)
       d = sets.get(j)
       if not d:
@@ -89,9 +133,15 @@ class union_find(object):
 
   def __contains__(self,seq):
     '''Returns true if all items in seq belong to the same set'''
+    if len(seq) < 2:
+      return True
     seq = iter(seq)
     j = self.find(seq.next(),False)
     return all(j == self.find(i,False) for i in seq)
+
+  def __iter__(self):
+    '''Iterator over all items seen'''
+    return iter(self.labels)
 
 
 def build_dupsets(dups):
