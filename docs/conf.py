b83a9fa7a8c5bc22f6f43baf23eb22baee26a00d
jacobske <jacobske@localhost>
2008-02-08 18:57:06 +0000

    Incremental improvements to documentation generation infrastructure

--- docs/conf.py
+++ docs/conf.py
@@ -1,23 +1,30 @@
 # -*- coding: utf-8 -*-
 #
-# GLU documentation build configuration file
+# Python documentation build configuration file
+#
+# This file is execfile()d with the current directory set to its containing dir.
 #
 # The contents of this file are pickled, so don't put values in the namespace
 # that aren't pickleable (module imports are okay, they're removed automatically).
-#
 
-import os
+import sys, os, time
+sys.path.append('tools/sphinxext')
+
+# General configuration
+# ---------------------
+
+extensions = ['sphinx.addons.refcounting']
+
+# General substitutions.
+project = 'GLU'
+copyright = '2008 BioInformed LLC'
 
 # The default replacements for |version| and |release|.
-# If 'auto', Sphinx looks for the Include/patchlevel.h file in the current Python
-# source tree and replaces the values accordingly.
 #
 # The short X.Y version.
-# version = '2.6'
-version = '0.81'
+version = '1.0'
 # The full version, including alpha/beta/rc tags.
-# release = '2.6a0'
-release = '0.81'
+release = '1.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -26,22 +33,14 @@ today = ''
 today_fmt = '%B %d, %Y'
 
 # Set templates to the local path
-templates_path=os.path.join(os.getcwd(), 'templates')
-
-# The base URL for download links.
-html_download_base_url = 'http://cgf.nci.nih.gov/glu/'
+templates_path=[os.path.join(os.getcwd(), 'templates')]
 
 # List of files that shouldn't be included in the build.
 unused_files = [
 ]
 
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-last_updated_format = '%b %d, %Y'
-
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-use_smartypants = True
+# Relative filename of the reference count data file.
+refcount_file = 'data/refcounts.dat'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
@@ -49,3 +48,59 @@ add_function_parentheses = True
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
 add_module_names = True
+
+
+# Options for HTML output
+# -----------------------
+
+# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
+# using the given strftime format.
+html_last_updated_fmt = '%b %d, %Y'
+
+# If true, SmartyPants will be used to convert quotes and dashes to
+# typographically correct entities.
+html_use_smartypants = True
+
+# Content template for the index page, filename relative to this file.
+#html_index = 'tools/sphinxext/indexcontent.html'
+
+# Custom sidebar templates, filenames relative to this file.
+html_sidebars = {
+#    'index': 'tools/sphinxext/indexsidebar.html',
+}
+
+# Additional templates that should be rendered to pages.
+html_additional_pages = {
+#    'download': 'tools/sphinxext/download.html',
+}
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'pydoc'
+
+
+# Options for LaTeX output
+# ------------------------
+
+# The paper size ('letter' or 'a4').
+latex_paper_size = 'letter'
+
+# The font size ('10pt', '11pt' or '12pt').
+latex_font_size = '10pt'
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, document class [howto/manual]).
+_stdauthor = r'Guido van Rossum\\Fred L. Drake, Jr., editor'
+latex_documents = []
+
+# Collect all HOWTOs individually
+
+# Additional stuff for the LaTeX preamble.
+latex_preamble = r'''
+\authoraddress{
+  \strong{GLU}\\
+  Email: \email{GLU@gmail.com}
+}
+'''
+
+# Documents to append as an appendix to all manuals.
+latex_appendices = []
