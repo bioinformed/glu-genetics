b7cba3fc74c090821c2547f8e045825f5bf6468f
jacobske <jacobske@localhost>
2008-04-08 18:08:11 +0000

    Add a new module util.table to expose the flexibility of GLU's load_table and table_writer APIs at the commandline level.
    Example:
    
      Extract two named columns from a compressed tab-delimited file and output
      to an uncompressed comma-delimited file:
    
      > glu util.table foo.gz:c=SAMPLE_ID,CGEMS_ID -o map.out:dialect=csv

---
+++ glu/modules/util/table.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+'''
+File:          join.py
+
+Authors:       Kevin Jacobs (jacobske@bioinformed.com)
+
+Created:       2008-03-06
+
+Abstract:      Toy module to read and write GLU tables
+
+Requires:      Python 2.5, glu
+
+Revision:      $Id$
+'''
+
+__copyright__ = 'Copyright (c) 2008, BioInformed LLC and the U.S. Department of Health & Human Services. Funded by NCI under Contract N01-CO-12400.'
+__license__   = 'See GLU license for terms by running: glu license'
+
+import sys
+
+from   glu.lib.fileutils import load_table,table_writer
+
+
+def option_parser():
+  import optparse
+  usage = 'Usage: %prog [options] table list1 [list2]...'
+
+  parser = optparse.OptionParser(usage=usage)
+
+  parser.add_option('-o', '--output', dest='output', metavar='FILE', default='-',
+                    help='Output results (default is "-" for standard out)')
+  return parser
+
+
+def main():
+  parser = option_parser()
+  options,args = parser.parse_args()
+
+  if len(args) != 1:
+    parser.print_help()
+    return
+
+  table = load_table(args[0],hyphen=sys.stdin,want_header=True)
+  out   = table_writer(options.output,hyphen=sys.stdout)
+
+  for row in table:
+    out.writerow(row)
+
+
+if __name__=='__main__':
+  main()
