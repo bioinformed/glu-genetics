c3dfa71f724a1410d39d56739790f1c4cb4e1eab
jacobske <jacobske@localhost>
2008-04-16 14:26:29 +0000

    Almost a complete rewrite of util.join in order to:
      1. perform inner and left outer joins (-j/--join)
      2. support equi-join semantics instead of assuming default key as the
         first column
      3. handle compound (multi-column) keys
      4. allow configurable null data values (not currently exposed to the commandline)
      5. enforce key uniqueness of the right table (-u/--unique)
      6. refactor into a general function and script driver
      7. first-cut of docstrings and doctests
    
    Backward incompatible changes to the previous development version:
    
      1. The '-s' option that removed key columns from table2 is now standard
         and non-configurable.
      2. The old implementation did not preserve table2 rows with equal keys,
         taking only the last.  This has been changed to always respect key
         cardinality, but raise an error if unique mappings are required
         (-u/--unique)
    
    At this point, I consider util.join to be a nearly final pending user
    feedback and stable enough for supervised production use.  In other words, I
    am confident enough to use it for doing real work, but the first few times
    be sure to check that it gives the expected answers.  My own verification
    has been using 5-way joins in PanScan analysis build 3 and comparing the
    results to both a custom script and to the results of a manual VLOOKUP hack
    in Excel.
    
    (Interestingly enough, I did find minor descrepencies in my early testing,
    but it turned out that util.join was correct and my custom script and Excel
    exhibited various glitches.  Take home message: 5-way joins aren't something
    to routinely roll by hand, even for the wary.)

--- glu/modules/util/join.py
+++ glu/modules/util/join.py
@@ -6,7 +6,7 @@ Authors:       Kevin Jacobs (jacobske@bioinformed.com)
 
 Created:       2008-03-06
 
-Abstract:      Utility to join two files on a common lookup key
+Abstract:      Utility to join two data tables on a equal row keys
 
 Requires:      Python 2.5, glu
 
@@ -16,45 +16,213 @@ Revision:      $Id$
 __copyright__ = 'Copyright (c) 2008, BioInformed LLC and the U.S. Department of Health & Human Services. Funded by NCI under Contract N01-CO-12400.'
 __license__   = 'See GLU license for terms by running: glu license'
 
+
 import sys
 
-from   glu.lib.fileutils import load_table,tryint,table_writer
+from   collections       import defaultdict
+
+from   glu.lib.utils     import pick,unique
+from   glu.lib.fileutils import load_table,tryint,table_writer,resolve_column_headers
 
 
 def option_parser():
   import optparse
-  usage = 'Usage: %prog [options] table list1 [list2]...'
+  usage = 'Usage: %prog [options] table1 table2'
 
   parser = optparse.OptionParser(usage=usage)
 
   parser.add_option('-o', '--output', dest='output', metavar='FILE', default='-',
                     help='Output results (default is "-" for standard out)')
+
   parser.add_option('-1', '--key1', dest='key1',
-                    help='Key column number or name')
+                    help='Table 1 key column numbers or names.  Comma separated, with '
+                         'default to common headers')
   parser.add_option('-2', '--key2', dest='key2',
-                    help='Key column number or name')
-  parser.add_option('-s', '--skipkey', dest='skipkey', action='store_true',
-                    help='Skip key2 in output')
+                    help='Table 2 key column numbers or names')
+  parser.add_option('-u', '--unique', dest='unique', action='store_true',
+                    help='Require that rows with valid keys from table2 are unique')
+  parser.add_option('-j', '--join', dest='join', default='left',
+                    help="Join type: 'left' (default) or 'inner'")
+
   return parser
 
 
-def strip(seq):
-  return (i.strip() for i in seq)
+def _parse_header(table,key):
+  table = iter(table)
 
+  try:
+    header = table.next()
+  except StopIteration:
+    return [],[],None
 
-def load(filename,key):
-  table  = load_table(filename,hyphen=sys.stdin,want_header=True)
-  header = table.next()
-  table  = list(table)
+  if isinstance(key, str):
+    key = [ tryint(k) for k in key.split(',') ]
+  elif isinstance(key, int):
+    key = [ key ]
 
-  key = tryint(key or 0)
-  if isinstance(key, basestring):
-    try:
-      key = header.index(key)
-    except ValueError:
-      raise ValueError("Cannot find header '%s'" % key)
+  if key is not None:
+    key = tuple(unique(resolve_column_headers(header,key)))
 
   return header,table,key
+
+
+def left_join(table1,table2,key1=None,key2=None,unique=False,inner=False,null=''):
+  '''
+  A generator that performs a relational join on two tables streams based on
+  key equality (see http://en.wikipedia.org/wiki/Join_(SQL) ).  Both input
+  tables are required to be an iterable sequence of a header and subsequent
+  data rows, all of equal length.
+
+  If no keys are specified, an equi-join is assumed on all matching headers.
+  Otherwise, keys may be specified as:
+
+  1) A sequence of header names, integer column indices (zero-based), string
+     column indices (zero-based)
+
+  or
+
+  2) a single comma delimited string of header names or column indices
+     (zero-based)
+
+  If inner is False, then a relational LEFT OUTER JOIN is performed such
+  that all rows in table1 will appear at least once in the results.  table1
+  rows without any matching table2 rows will be emitted with all null values
+  for table2 fields.
+
+  If inner is True, then a relational INNER JOIN is performed such that only
+  rows in table1 with one or more matching rows in table2 will be emitted.
+
+  Row keys containing null values, as determined by the null parameter, will
+  not match any other key.  All keys must be hashable since the underlying
+  implementation is a hash join (see
+  http://en.wikipedia.org/wiki/Hash_join).
+
+  The results contain all fields from table 1 and all non-key fields from
+  table2 and begins with a header and each subsequent result data row.
+
+  @param table1: Left input table
+  @type  table1: Sequence of header and data rows, all of equal row length
+  @param table2: Right input table
+  @type  table2: Sequence of header and data rows, all of equal row length
+  @param   key1: Key selector for table1, if None an equi-join is performed
+  @type    key1: Sequence of mixed int or str, or str, or None
+  @param   key2: Key selector for table1, if None an equi-join is performed
+  @type    key2: Sequence of mixed int or str, or str, or None
+  @param unique: If True, raise an exception if table2 keys are not unique
+  @type  unique: bool
+  @param  inner: If True perform a INNER JOIN, otherwise a LEFT OUTER JOIN
+  @type   inner: bool
+  @param   null: Null data value
+  @type    null: object
+  @return      : join results with header and data rows
+  @rtype       : generator
+
+  >>> table1 = [[ 'A', 'B'],
+  ...           [   1,   2],
+  ...           [   1,   3],
+  ...           [   2,   1],
+  ...           [None,None]]
+  >>> table2 = [[ 'B', 'C'],
+  ...           [   3,   1],
+  ...           [None,   1],
+  ...           [   2,   0]]
+
+  Left outer equi-join:
+
+  >>> for row in left_join(table1,table2,null=None):
+  ...   print row
+  ['A', 'B', 'C']
+  [1, 2, 0]
+  [1, 3, 1]
+  [2, 1, None]
+  [None, None, None]
+
+  Left outer join with specified keys:
+
+  >>> for row in left_join(table1,table2,key1=1,key2=0,null=None):
+  ...   print row
+  ['A', 'B', 'C']
+  [1, 2, 0]
+  [1, 3, 1]
+  [2, 1, None]
+  [None, None, None]
+
+  Inner equi-join:
+
+  >>> for row in left_join(table1,table2,null=None,inner=True):
+  ...   print row
+  ['A', 'B', 'C']
+  [1, 2, 0]
+  [1, 3, 1]
+  '''
+  header1,table1,index1 = _parse_header(table1, key1)
+  header2,table2,index2 = _parse_header(table2, key2)
+
+  if index1 is None:
+    index1 = index2
+  if index2 is None:
+    index2 = index1
+
+  if index1 is None and index2 is None:
+    equikeys = set(header1) & set(header2)
+    index1 = tuple(i for i,h in enumerate(header1) if h in equikeys)
+    index2 = tuple(i for i,h in enumerate(header2) if h in equikeys)
+
+  if not index1 or not index2:
+    raise ValueError('No compatible keys found')
+
+  if len(index1) != len(index2):
+    raise ValueError('Keys must be of same length')
+
+  index2r   = [ i for i in range(len(header2)) if i not in index2 ]
+  header2   = pick(header2,index2r)
+  len1,len2 = len(header1),len(header2)
+  max1,max2 = max(index1),max(index2)
+
+  # Compute the left hash-join
+
+  # Step 1: Extract keys and hash all rows in table2
+  rowmap = defaultdict(list)
+  for row in table2:
+    # Skip the row if it is too short to have all keys
+    if len(row) <= max2:
+      continue
+
+    key = tuple(pick(row,index2))
+
+    # Keys are null values treated as NULL
+    if any(k==null for k in key):
+      continue
+
+    if unique and row in rowmap[key]:
+      raise ValueError('Non-unique key %s' % (key,))
+
+    # Extract non-key fields and normalize length
+    row  = pick(row,index2r)
+    row += [null]*(len2-len(row))
+    row  = row[:len2]
+
+    rowmap[key].append(row)
+
+  # Step 2: Perform the join over all rows in table 1
+
+  # Yield the new header
+  yield header1 + header2
+
+  # This default value for non-key matches is what enforces the LEFT OUTER
+  # or INNER JOIN semantics.
+  blank2 = [[null]*len2] if not inner else []
+
+  for row in table1:
+    key  = tuple(pick(row,index1)) if len(row) > max1 else None
+
+    # Normalize length
+    row += [null]*(len1-len(row))
+    row  = row[:len1]
+
+    # Combine with
+    for row2 in rowmap.get(key,blank2):
+      yield row+row2
 
 
 def main():
@@ -65,30 +233,21 @@ def main():
     parser.print_help()
     return
 
-  lheader,ltable,key1 = load(args[0],options.key1)
-  rheader,rtable,key2 = load(args[1],options.key2)
+  join_type = options.join.lower()
+  if join_type not in ('left','inner'):
+    raise ValueError('Invalid join type specified: %s' % join_type)
 
-  # FIXME: Check hash-join is unique or handle non-unique!!!
-  n = len(rheader)
-  if not options.skipkey:
-    rmap = dict( (row[key2].strip(),row) for row in rtable )
-  else:
-    rmap = dict( (row[key2].strip(),row[:key2]+row[key2+1:]) for row in rtable )
-    rheader = rheader[:key2]+rheader[key2+1:]
-    n -= 1
+  table1 = load_table(args[0],hyphen=sys.stdin,want_header=True)
+  table2 = load_table(args[1],hyphen=sys.stdin,want_header=True)
 
-  out = table_writer(options.output,hyphen=sys.stdout)
+  table  = left_join(table1,table2,key1=options.key1,key2=options.key2,
+                    unique=options.unique,inner=(join_type=='inner'))
+  out    = table_writer(options.output,hyphen=sys.stdout)
 
-  out.writerow(lheader + rheader)
-
-  blank = ['']*n
-
-  for lrow in ltable:
-    key  = lrow[key1].strip()
-    rrow = rmap.get(key,blank)
-
-    out.writerow(lrow+rrow)
+  out.writerows(table)
 
 
 if __name__=='__main__':
+  import doctest
+  doctest.testmod()
   main()
