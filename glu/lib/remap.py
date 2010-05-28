fb8b5230344f1ee01c5b3c3c08d2bfcafa5dcc22
Kevin B. Jacobs <jacobs@bioinformed.com>
2010-05-28 14:34:53 -0400

    Correct abstract text

--- glu/lib/remap.py
+++ glu/lib/remap.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__abstract__  = 'Requires:      Python 2.5, glu'
+__abstract__  = 'Remap alleles'
 __copyright__ = 'Copyright (c) 2007-2009, BioInformed LLC and the U.S. Department of Health & Human Services. Funded by NCI under Contract N01-CO-12400.'
 __license__   = 'See GLU license for terms by running: glu license'
 __revision__  = '$Id$'
