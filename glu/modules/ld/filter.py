25b2000e5f454d157a0f1ec11281e1563297a11f
Kevin B. Jacobs <jacobs@bioinformed.com>
2011-11-28 14:55:50 -0500

    Implement new 2 hit LD filter

--- glu/modules/ld/filter.py
+++ glu/modules/ld/filter.py
@@ -11,11 +11,13 @@ __revision__  = '$Id$'
 
 import sys
 
-from   itertools               import islice
+from   itertools                 import islice
 
-from   glu.lib.fileutils       import table_reader, table_writer, resolve_column_header
-from   glu.lib.genolib         import load_genostream, geno_options
-from   glu.lib.genolib.ld      import count_haplotypes, estimate_ld
+from   glu.lib.fileutils         import table_reader, table_writer, resolve_column_header
+
+from   glu.lib.genolib           import load_genostream, geno_options
+from   glu.lib.genolib.transform import _intersect_options, _union_options
+from   glu.lib.genolib.ld        import count_haplotypes, estimate_ld
 
 
 def option_parser():
@@ -28,8 +30,6 @@ def option_parser():
 
   geno_options(parser,input=True,filter=True)
 
-  parser.add_argument('-r', '--r2threshold', metavar='N', type=float, default=0.80,
-                          help='Minimum r-squared threshold (default=0.80)')
   parser.add_argument('-m', dest='maxdist', metavar='BASES', default=200000, type=int,
                     help='Maximum distance in bases between loci to apply LD check.  default=200000')
   parser.add_argument('--lheader', default='Locus',
@@ -38,6 +38,22 @@ def option_parser():
                           help='Filter the top N loci (default=0 for unlimited)')
   parser.add_argument('-o', '--output', metavar='FILE', default='-',
                     help='Output LD filter results to FILE')
+  parser.add_argument('-O', '--detailout', metavar='FILE',
+                    help='Output LD filter details to FILE')
+  parser.add_argument('--pheader',    default='score p-value',
+                          help='p value header column name or number (default=score p-value)')
+  parser.add_argument('--p1hit', metavar='P', type=float, default=1.0,
+                          help='Filter loci with p value lower than one-hit threshold (default=1.0)')
+  parser.add_argument('--p2hit', metavar='P', type=float, default=0.0,
+                          help='Filter loci with p value lower than two-hit threshold (default=0.0)')
+  parser.add_argument('--r1hit', metavar='N', type=float, default=0.80,
+                          help='Minimum one hit r-squared threshold (default=0.80)')
+  parser.add_argument('--r2hit', metavar='N', type=float, default=0.80,
+                          help='Minimum two hit r-squared threshold (default=0.80)')
+  parser.add_argument('--includedesign', metavar='FILE', action='append',
+                    help='List of loci that may be taken')
+  parser.add_argument('--excludedesign', metavar='FILE', action='append',
+                    help='List of loci that may not be taken')
 
   return parser
 
@@ -54,15 +70,37 @@ def main():
   parser = option_parser()
   options= parser.parse_args()
 
+  maxdist     = options.maxdist
+  take1r2     = options.r1hit
+  take2r2     = options.r2hit
+  take1p      = options.p1hit
+  take2p      = options.p2hit
+
+  if take1p<take2p:
+    raise ValueError('Two-hit p-value threshold must be less than the one-hit threshold (%f<%f)' % (take1p,take2p))
+
   rows   = table_reader(options.snplist,hyphen=sys.stdin)
   header = rows.next()
-  index  = resolve_column_header(header,options.lheader)
+  lindex = resolve_column_header(header,options.lheader)
+  pindex = resolve_column_header(header,options.pheader) if options.pheader else -1
 
   if options.limit:
     rows = islice(rows,options.limit)
 
-  rows   = list(rows)
-  snps   = set(row[index] for row in rows)
+  if pindex>=0 and take1p<1.0:
+    snps = set()
+    new_rows = []
+    for row in rows:
+      pval = float(row[pindex] or 1.0)
+      if pval>take1p:
+        break
+      new_rows.append(row)
+      snps.add(row[lindex])
+    rows = new_rows
+  else:
+    rows   = list(rows)
+    snps   = set(row[lindex] for row in rows)
+
   genos  = load_genostream(options.genotypes,format=options.informat,genorepr=options.ingenorepr,
                            genome=options.loci,phenome=options.pedigree,
                            transform=options, hyphen=sys.stdin)
@@ -73,22 +111,46 @@ def main():
 
   out.writerow(header+['LDFILTER_RANK','LDFILTER_RANK_TAKEN','LDFILTER_TAKEN','LDFILTER_REASON','LDFILTER_DETAILS'])
 
-
-  maxdist     = options.maxdist
-  r2threshold = options.r2threshold
   taken       = []
   takenset    = set()
+  twohitset   = set()
+  twohit      = 0
   skippedld   = 0
   skippedt    = 0
+  excluded    = 0
   missingdata = 0
   genos       = dict(genos)
 
+  include = _intersect_options(options.includedesign or [])
+  exclude =     _union_options(options.excludedesign or []) or set()
+
+  if include is None:
+    include = snps
+  else:
+    exclude |= snps-include
+
+  r2threshold = take2r2
+  pval = 1.0
+
   for i,row in enumerate(rows):
-    locus = row[index]
+    locus = row[lindex]
+
+    if pindex>=0:
+      pval = float(row[pindex] or 1.0)
+
+      if pval>take1p:
+        break
+      elif pval>take2p:
+        r2threshold = take1r2
 
     if locus in takenset:
       skippedt += 1
       out.writerow(row+[i+1,'','SKIP','ALREADY_TAKEN',''])
+      continue
+
+    if locus in exclude:
+      excluded += 1
+      out.writerow(row+[i+1,'','SKIP','SNP EXCLUDED FROM CONSIDERATION',''])
       continue
 
     if locus not in genos:
@@ -117,25 +179,47 @@ def main():
       if r2>=r2threshold:
         snpld.append( (tlocus,r2) )
 
+    hits2  = set(tlocus for tlocus,r2 in snpld if r2>=take2r2)
+    hits2 &= twohitset
+
     if not near:
       taken.append(locus)
       takenset.add(locus)
-      out.writerow(row+[i+1,len(taken),'TAKE','No SNPs nearby when selected',''])
+      out.writerow(row+[i+1,len(taken), 'TAKE', 'No SNPs nearby when selected', ''])
     elif not snpld:
       taken.append(locus)
       takenset.add(locus)
-      out.writerow(row+[i+1,len(taken),'TAKE','Below LD threshold', '%d SNPs nearby' % len(near) ])
+      if pval<=take2p:
+        twohitset.add(locus)
+      out.writerow(row+[i+1,len(taken), 'TAKE', 'Below LD threshold', '%d SNPs nearby' % len(near)])
+    elif hits2:
+      twohit += 1
+      taken.append(locus)
+      takenset.add(locus)
+      twohitset -= hits2
+      hits2 = ','.join(sorted(hits2))
+      out.writerow(row+[i+1,len(taken), 'TAKE', 'Second hit', 'First hit(s): %s' % hits2])
     else:
       skippedld += 1
       firstloc,firstr2 = snpld[0]
       out.writerow(row+[i+1,'','SKIP','Above LD threshold',
                                'First: %s r2=%.2f, Others: %d' % (firstloc,firstr2,(len(snpld)-1))])
 
-  print >> sys.stderr, 'Skip Taken    :',skippedt
-  print >> sys.stderr, 'Skip LD       :',skippedld
-  print >> sys.stderr, 'Taken         :',len(takenset)
-  print >> sys.stderr, '  Has ref data:',len(taken)
-  print >> sys.stderr, '  No ref data :',missingdata
+  print >> sys.stderr, 'Skip Excluded     :',excluded
+  print >> sys.stderr, 'Skip already taken:',skippedt
+  print >> sys.stderr, 'Skip LD           :',skippedld
+  print >> sys.stderr, 'Take              :',len(takenset)
+  print >> sys.stderr, '  Has ref data    :',len(taken)
+  print >> sys.stderr, '  No ref data     :',missingdata
+  print >> sys.stderr, '  2-hit extra     :',twohit
+  print >> sys.stderr, 'Missing 2 hit     :',len(twohitset)
+
+  if options.detailout:
+    out = table_writer(options.detailout)
+
+    out.writerow(['LOCUS','STATUS'])
+    for locus in sorted(twohitset):
+      out.writerow([locus,'Missing second surrogate'])
 
 
 if __name__ == '__main__':
